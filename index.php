b6ff187f445629fda23c9138ad90fe747821c78e
deanrobertcook <deanrobertcook@gmail.com>
2014-11-20 19:09:34 +0800

    Remove old app and restart workflow

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * Blank index.php file for a new website/app
- * There are also blank template folders to avoid having to refind all of these basic parts to an app.
- */
-require_once 'core/init.php';
-$router = new Router();
-$router->runController();
