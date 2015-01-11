5b46ea40d74e76018947979004c75c3fffa0ca82
deanrobertcook <deanrobertcook@gmail.com>
2015-01-11 18:33:20 +0100

    Lexemes and meanings included in initial payload

--- module/Lexemes/src/Lexemes/Controller/IndexController.php
+++ module/Lexemes/src/Lexemes/Controller/IndexController.php
@@ -8,6 +8,15 @@ use Zend\Mvc\Controller\AbstractActionController;
 class IndexController extends AbstractActionController {
 
 	public function indexAction() {
-		return array();
+		$meaningService = $this->serviceLocator->get('meaningService');
+		$lexemeService = $this->serviceLocator->get('lexemeService');
+		
+		$meanings = json_encode($meaningService->getAllMeanings('de', 'en'));
+		$lexemes = json_encode($lexemeService->getAllLexemes('de', 'en'));
+		
+		return array(
+			'meanings' => $meanings,
+			'lexemes' => $lexemes
+		);
 	}
 }
