49c8f704c8e7708c3eaeaf75579d461380047e6c
deanrobertcook <deanrobertcook@gmail.com>
2015-01-11 20:11:47 +0100

    Added info bar back to meanings list

--- public/js/lexemes/views/meanings.js
+++ public/js/lexemes/views/meanings.js
@@ -2,6 +2,9 @@ var app = app || {};
 
 app.MeaningsView = Backbone.View.extend({
 	tagName: 'div',
+	
+	newMeaningFormTemplate: _.template($('#newMeaning').html()),
+	infoTemplate: _.template($('#meaningsInfo').html()),
 
 	initialize: function(initialMeanings) {
 		this.collection = new app.Meanings(initialMeanings);
@@ -9,12 +12,18 @@ app.MeaningsView = Backbone.View.extend({
 	
 	render: function() {
 		this.$el.empty();
+		this.renderInfoBar();
 		this.collection.each(function(meaning) {
 			this.renderMeaning(meaning);
 		}, this);
 		$("#lexicon").html(this.$el);
 	},
 	
+	renderInfoBar: function() {
+		this.$el.append(this.infoTemplate({"meaningCount": this.collection.length}));
+		return this;
+	},
+
 	renderMeaning: function(meaning) {
 		var meaningView = new app.MeaningView({
 			model: meaning,
