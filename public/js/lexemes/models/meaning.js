003f820939a38cb5762c0f570aad277eae95d121
deanrobertcook <deanrobertcook@gmail.com>
2015-01-11 21:13:50 +0100

    Added frequency and dateEntered defaults for the meaning model may need to work on the format of the date

--- public/js/lexemes/models/meaning.js
+++ public/js/lexemes/models/meaning.js
@@ -1,4 +1,12 @@
 var app = app || {};
 
 app.Meaning = Backbone.Model.extend({
+	defaults: {
+		frequency: 1,
+		dateEntered: this.getDate(),
+	},
+	
+	getDate: function() {
+		return Date.now();
+	}
 });
