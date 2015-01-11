002a615fbe1de85791e6838073ccf6c1c9a79fa4
deanrobertcook <deanrobertcook@gmail.com>
2015-01-11 18:02:08 +0100

    Removed lexeme rendering from mening rendering until I get it working again

--- public/js/lexemes/views/meaning.js
+++ public/js/lexemes/views/meaning.js
@@ -6,21 +6,7 @@ app.MeaningView = Backbone.View.extend({
 	template: _.template($('#meaningTemplate').html()),
 	
 	render: function() {
-		var targetLexeme = this.model.get('targetLexeme');
-		targetLexeme.target = true;
-		var targetView = new app.LexemeView({
-			model: new app.Lexeme(targetLexeme),
-		});
-		
-		var baseLexeme = this.model.get('baseLexeme');
-		baseLexeme.target = false;
-		var baseView = new app.LexemeView({
-			model: new app.Lexeme(baseLexeme),
-		});
-		
-		this.$el.append(targetView.render().el);
-		this.$el.append(baseView.render().el);
-		this.$el.append(this.template(this.model.attributes));
+		this.$el.html(this.template(this.model.attributes));
 		return this;
 	}
 });
