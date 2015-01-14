ffe86b08f45bec08e0ac607d3bd47670724b6339
deanrobertcook <deanrobertcook@gmail.com>
2015-01-14 16:21:28 +0100

    Installed backbone pagination

--- public/js/lexemes/collections/meanings.js
+++ public/js/lexemes/collections/meanings.js
@@ -1,28 +1,29 @@
-myLexicon.CollectionClasses.Meanings = Backbone.Collection.extend({
+myLexicon.CollectionClasses.Meanings = Backbone.PageableCollection.extend({
 	model: myLexicon.ModelClasses.Meaning,
 	url: 'meanings',
 	
-	createMeaning: function(formData) {
+	
+	createMeaning: function (formData) {
 		var targetData = {
-				"language": "de", //Change this to default to user's selection
-				"type": formData.targetType,
-				"entry": formData.targetEntry,
+			"language": "de", //Change this to default to user's selection
+			"type": formData.targetType,
+			"entry": formData.targetEntry,
 		};
 
 		var baseData = {
-				"language": "en", //Change this to default to user's selection
-				"type": formData.baseType,
-				"entry": formData.baseEntry,
+			"language": "en", //Change this to default to user's selection
+			"type": formData.baseType,
+			"entry": formData.baseEntry,
 		};
-		
+
 		var targetcid = myLexicon.Collections.lexemes.createNewLexeme(targetData);
 		var basecid = myLexicon.Collections.lexemes.createNewLexeme(baseData);
-		
+
 		var meaning = new myLexicon.ModelClasses.Meaning({
 			"targetid": targetcid,
 			"baseid": basecid
 		});
-		
+
 		this.add(meaning);
 		meaning.pushLexemes();
 	}
