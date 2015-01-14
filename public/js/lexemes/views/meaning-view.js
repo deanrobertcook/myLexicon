89c1202bda3905cb6623315a9e8aeabdadebe5ef
deanrobertcook <deanrobertcook@gmail.com>
2015-01-14 14:57:07 +0100

    Removed uncessary functions from meaningView

--- public/js/lexemes/views/meaning-view.js
+++ public/js/lexemes/views/meaning-view.js
@@ -3,20 +3,6 @@ myLexicon.ViewClasses.MeaningView = Backbone.View.extend({
 	className: 'meaningContainer',
 	template: _.template($('#meaningTemplate').html()),
 	
-	initialize: function() {
-		var targetLexeme = myLexicon.Collections.lexemes.findLexeme(this.model.get('targetid'));
-		var baseLexeme = myLexicon.Collections.lexemes.findLexeme(this.model.get('baseid'));
-		
-		this.listenTo(targetLexeme, 'change:[id]', this.changedLexeme);
-		this.listenTo(baseLexeme, 'change:[id]', this.changedLexeme);
-		this.listenTo(this.model, 'sync', this.meaningSynced);
-	},
-	
-	meaningSynced: function(meaning, response) {
-		this.model.set('id', response.id);
-		this.render();
-	},
-	
 	render: function() {
 		this.$el.empty();
 		this.renderLexeme(this.model.get('targetid'));
