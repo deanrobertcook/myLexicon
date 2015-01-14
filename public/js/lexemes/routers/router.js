3c8d71758e58c706701d9e2cff701f63abc2885a
deanrobertcook <deanrobertcook@gmail.com>
2015-01-14 14:55:33 +0100

    Added comments to router class

--- public/js/lexemes/routers/router.js
+++ public/js/lexemes/routers/router.js
@@ -10,17 +10,30 @@ myLexicon.RouterClasses.Router = Backbone.Router.extend({
 		this.getAllMeanings();
 	},
 	
+	/**
+	 * Shows all bare lexemes within the users main collection without any links
+	 * to any meanings
+	 */
 	getAllLexemes: function () {
 		var lexemesView = new myLexicon.ViewClasses.LexemesView(myLexicon.Collections.lexemes);
 		lexemesView.render();
 	},
 	
+	/**
+	 * For a given lexeme, all associated meanings are displayed from the main
+	 * collection
+	 * @param {int} id
+	 */
 	displayLexeme: function(id) {
 		var lexeme = myLexicon.Collections.lexemes.findLexeme(id);
 		var lexemeView = new myLexicon.ViewClasses.LexemeView({model: lexeme});
 		lexemeView.displayLexeme();
 	},
 	
+	/**
+	 * Shows all meanings (and their associated lexemes) that are stored in the 
+	 * user's main collection
+	 */
 	getAllMeanings: function () {
 		var meaningsView = new myLexicon.ViewClasses.MeaningsView(myLexicon.Collections.meanings);
 		meaningsView.render();
