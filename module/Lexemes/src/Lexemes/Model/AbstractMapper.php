06734ccc85636851bce259d552e5d652405dd392
deanrobertcook <deanrobertcook@gmail.com>
2015-01-18 16:44:14 +0100

    Abstract mapper insert functionality completed with checkIfExists

--- module/Lexemes/src/Lexemes/Model/AbstractMapper.php
+++ module/Lexemes/src/Lexemes/Model/AbstractMapper.php
@@ -16,14 +16,40 @@ class AbstractMapper
 
 	public function select($sql, $params)
 	{
-		$resultSet = $this->adapter->query($sql, $params);
+		$statement = $this->adapter->createStatement($sql);
+		$resultSet = $statement->execute($params);
 		$results = null;
 		if ($resultSet->count() == 1) {
-			$results = $resultSet->toArray()[0];
+			$results = $resultSet->current();
 		} else {
-			$results = $resultSet->toArray();
+			$results = $this->getAllResults($resultSet);
 		}
 		return $results;
+	}
+	
+	private function getAllResults($resultSet) {
+		$count = $resultSet->count();
+		$results = array();
+		for ($i = 0; $i < $count; $i++) {
+			$results[] = $resultSet->current();
+		}
+		return $results;
+	}
+	
+	public function checkIfExists($sql, $params) {
+		$result = $this->select($sql, $params);
+		if (isset($result['id'])) {
+			return $result['id'];
+		} else {
+			return false;
+		}
+	}
+	
+	public function insert($sql, $params) {
+		$statement = $this->adapter->createStatement($sql);
+		$resultSet = $statement->execute($params);
+		$id = $resultSet->getGeneratedValue();
+		return $id;
 	}
 
 }
