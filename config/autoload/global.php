6819f4a9a52eb27b42b56752bd215e6daaf60859
deanrobertcook <deanrobertcook@gmail.com>
2015-01-18 18:26:58 +0100

    User module and database now in harmony

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -15,14 +15,17 @@ return array(
 	'db' => array(
 		'driver' => 'PdoMysql',
 		'hostname' => 'localhost',
-		'database' => 'myLexiconTest',
+		'database' => 'myLexicon',
 		'driver_options' => array(
 			PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
 		),
 	),
 	'service_manager' => array(
 		'factories' => array(
-			'dbAdapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+			'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
 		),
+		'aliases' => array(
+            'dbAdapter' => 'Zend\Db\Adapter\Adapter',
+        ),
 	),
 );
