44c4fe960c48280ebbfd2747080211b0e81fb0ed
deanrobertcook <deanrobertcook@gmail.com>
2015-02-12 18:58:46 +0100

    Fixed bug in lexicon view that meant duplicates of meanings based on frequency

--- public/js/lexemes/routers/router.js
+++ public/js/lexemes/routers/router.js
@@ -10,33 +10,7 @@ myLexicon.RouterClasses.Router = Backbone.Router.extend({
 	},
 	
 	default: function () {
-		var collection = myLexicon.Collections.meanings;
-		collection.comparator = 'id';
-		collection.sort();
-		
-//		console.log(collection.length);
-		
-		var meanings = allMeanings;
-		meanings.sort(function(a, b) {
-			if (a.id > b.id) {
-				return 1;
-			}
-			if (a.id < b.id) {
-				return -1;
-			}
-			
-			return 0;
-		});
-//		console.log(meanings);
-		var numberDupped = 0;
-		for (var i = 0; i < meanings.length; i++) {
-			if (i > 0) {
-				if (meanings[i].id === meanings[i-1].id) {
-					numberDupped++;
-				}
-			}
-		}
-		console.log(numberDupped);
+		this.meanings();
 	},
 	
 	/**
